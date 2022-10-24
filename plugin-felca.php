f97595b290b9b8d04ec16526e85b5a2efaf74f1f
Felcagod <100715107+Felcagod@users.noreply.github.com>
2022-10-24 00:35:49 -0300

    Add files via upload
    Codigo bem simples para adicionar ou tirar a barra de Admin para o front-end

---
+++ plugin-felca.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Plugin Felca
+ * Pluigin URI: 
+ * Description: Esse e o meu novo plugin.
+ * Version: 0.1
+ * Author: Wagner Paulo
+ * Author URI: 
+ */
+
+ add_filter( 'show_admin_bar' , '__return_false');
+
+
